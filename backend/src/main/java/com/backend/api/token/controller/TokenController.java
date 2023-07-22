08ef745dda4fd38ae7a63083550c70bbfab497c3
최영태 <dudxo7721@naver.com>
2023-07-23 00:56:31 +0900

    JWT

---
+++ backend/src/main/java/com/backend/api/token/controller/TokenController.java
@@ -0,0 +1,31 @@
+package com.backend.api.token.controller;
+
+import com.backend.api.token.dto.AccessTokenResponseDto;
+import com.backend.api.token.service.TokenService;
+import com.backend.global.util.AuthorizationHeaderUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class TokenController {
+
+    private final TokenService tokenService;
+
+    // refresh token 날아감
+    @PostMapping("/access-token/issue")
+    public ResponseEntity<AccessTokenResponseDto> createAccessToken(HttpServletRequest httpServletRequest) {
+        String authorizationHeader = httpServletRequest.getHeader("Authorization");
+        AuthorizationHeaderUtils.validateAuthorization(authorizationHeader);
+
+        String refreshToken = authorizationHeader.split(" ")[1];
+        AccessTokenResponseDto accessTokenResponseDto = tokenService.createAccessTokenByRefreshToken(refreshToken);
+        return ResponseEntity.ok(accessTokenResponseDto);
+    }
+}

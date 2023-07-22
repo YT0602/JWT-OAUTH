7cd074475005aacee9d21a8a011f3f238d3287e0
최영태 <dudxo7721@naver.com>
2023-07-23 00:58:51 +0900

    interceptor, resolver

---
+++ backend/src/main/java/com/backend/global/resolver/memberInfo/MemberInfoArgumentResolver.java
@@ -0,0 +1,47 @@
+package com.backend.global.resolver.memberInfo;
+
+import com.backend.domain.member.constant.Role;
+import com.backend.global.jwt.service.TokenManager;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@RequiredArgsConstructor
+public class MemberInfoArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final TokenManager tokenManager;
+
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasMemberInfoAnnotation = parameter.hasParameterAnnotation(MemberInfo.class);
+        boolean hasMemberInfoDto = MemberInfoDto.class.isAssignableFrom(parameter.getParameterType());
+        return hasMemberInfoAnnotation && hasMemberInfoDto;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String authorizationHeader = request.getHeader("Authorization");
+        String token = authorizationHeader.split(" ")[1];
+
+        Claims tokenClaims = tokenManager.getTokenClaims(token);
+        String email = (String) tokenClaims.get("email");
+        String role = (String) tokenClaims.get("role");
+
+        return MemberInfoDto.builder()
+                .email(email)
+                .role(Role.from(role))
+                .build();
+    }
+
+
+}
